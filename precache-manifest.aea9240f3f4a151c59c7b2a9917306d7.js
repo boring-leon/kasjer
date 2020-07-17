0036a8fc68484ca9e2fb0d94b0e6ccd97aec3be0
LeonCzerwinski <poriusz@wp.pl>
2020-07-18 01:32:12 +0200

    deploy

---
+++ precache-manifest.aea9240f3f4a151c59c7b2a9917306d7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a0eea108a0f6e5d67af31857705903",
+    "url": "/kasjer/app.css"
+  },
+  {
+    "revision": "42009637d16c91d9bec1",
+    "url": "/kasjer/css/app.43360186.css"
+  },
+  {
+    "revision": "2a78473dff2a2a8d73cc0c2f110ebcae",
+    "url": "/kasjer/img/100zl.2a78473d.jpg"
+  },
+  {
+    "revision": "17a57004658fd724b97541ef04135004",
+    "url": "/kasjer/img/10gr.17a57004.jpg"
+  },
+  {
+    "revision": "657e4da85f8182d2a08121af0dd0896a",
+    "url": "/kasjer/img/10zl.657e4da8.jpg"
+  },
+  {
+    "revision": "e18f368ce958f5b8cc6c83773ac5e846",
+    "url": "/kasjer/img/1gr.e18f368c.jpg"
+  },
+  {
+    "revision": "2a22374f9713210dca91c50948a4646e",
+    "url": "/kasjer/img/1zl.2a22374f.jpg"
+  },
+  {
+    "revision": "2c9ad34f1cb290007b7bd4c2e55017a7",
+    "url": "/kasjer/img/200zl.2c9ad34f.jpg"
+  },
+  {
+    "revision": "ecb1b860b0541f9896563609aecdabb4",
+    "url": "/kasjer/img/20gr.ecb1b860.jpg"
+  },
+  {
+    "revision": "cdced65c25f3c152e5ccd88623b1beeb",
+    "url": "/kasjer/img/20zl.cdced65c.jpg"
+  },
+  {
+    "revision": "0910ec5177eccd29d1cf70360ec2eb11",
+    "url": "/kasjer/img/2gr.0910ec51.jpg"
+  },
+  {
+    "revision": "958db1c6ad29b79b4d4c8dec8b34d5cb",
+    "url": "/kasjer/img/2zl.958db1c6.jpg"
+  },
+  {
+    "revision": "df31512567e1af4dcecfe02e09f3769e",
+    "url": "/kasjer/img/500zl.df315125.jpg"
+  },
+  {
+    "revision": "95c17be1a2957fec99d52b8308e8c78a",
+    "url": "/kasjer/img/50gr.95c17be1.jpg"
+  },
+  {
+    "revision": "9a8b73121d5a74d80c0dd15abc094add",
+    "url": "/kasjer/img/50zl.9a8b7312.jpg"
+  },
+  {
+    "revision": "bf25369c9c6703edaa72a4ee3666f899",
+    "url": "/kasjer/img/5gr.bf25369c.jpg"
+  },
+  {
+    "revision": "295a7163e9f8a15a53662c6f26d3554e",
+    "url": "/kasjer/img/5zl.295a7163.jpg"
+  },
+  {
+    "revision": "bcd44635f203eb61be0fc89e6443c882",
+    "url": "/kasjer/index.html"
+  },
+  {
+    "revision": "42009637d16c91d9bec1",
+    "url": "/kasjer/js/app.8e2b638a.js"
+  },
+  {
+    "revision": "16da19c8c18068ae783d",
+    "url": "/kasjer/js/chunk-vendors.3c30a6ba.js"
+  },
+  {
+    "revision": "207b734d690e13c9d07881bc995e0145",
+    "url": "/kasjer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kasjer/robots.txt"
+  }
+]);
